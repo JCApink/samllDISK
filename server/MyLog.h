01d2c37ccbfd1ea7e59eb4cb41e19543722ce2de
ubuntu <ubuntu@localhost.localdomain>
2021-02-09 22:55:17 +0800

    1.0

---
+++ server/MyLog.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "function.h"
+#include "MyDb.h"
+#define LOG(user, operation){MyLog log;log.insert(user, operation);}
+
+class MyLog
+{
+public:
+    MyLog() {
+        db.initDB("localhost", "root", "hws1866-", "Netdisk");
+    }
+
+    void insert(const string &user, const string &operation)
+    {
+        time_t t;
+        time(&t);
+        string stime(ctime(&t));
+        string sql = "INSERT INTO Log (User, Operation, time) VALUES('" + user + "', '" + operation + "', '" + stime + "')";
+        db.exeSQL(sql);
+    }
+
+private:
+    MyDb db;
+};
+
---
+++ server/send_recv_cycle.cpp
@@ -0,0 +1,27 @@
+#include "function.h"
+int recvCycle(int fd, void *p, size_t len) {
+    char *pStart = (char *)p;
+    size_t size = 0;
+    while (size < len) {
+        int ret = recv(fd, pStart + size, len - size, 0);
+        if (0 == ret)
+        {
+            return 0;
+        }
+        error_check(ret, -1, "recv");
+        size += ret;
+    }
+    return len;
+}
+
+int sendCycle(int fd, void *p, size_t len) {
+    char *pStart = (char *)p;
+    size_t size = 0;
+    while (size < len) {
+        int ret = send(fd, pStart + size, len - size, 0);
+        if (-1 == ret )
+            return -1;
+        size += ret;
+    }
+    return len;
+}
